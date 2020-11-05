6cd53d8dd057cab95b220acf80346051580107b3
travor20814 <travor20814@gmail.com>
2020-11-05 20:34:41 +0800

    feat(selector): add a simple selector

--- client/Views/Main/Landing.jsx
+++ client/Views/Main/Landing.jsx
@@ -2,17 +2,48 @@
 import React from 'react';
 import { css } from 'emotion';
 
+import Selector from '../Tools/Selector';
+
 const classes = {
   wrapper: css`
     width: 100%;
     height: auto;
+  `,
+  table: css`
+    width: 100%;
+    max-width: 720px;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+  `,
+  row: css`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 160px 1fr;
+    grid-column-gap: 24px;
+    align-items: center;
+  `,
+  rowLabel: css`
+    font-size: 16px;
+    font-weight: 500;
+    color: #1a1a1a;
+    letter-spacing: 1px;
   `,
 };
 
 function Landing() {
   return (
     <div className={classes.wrapper}>
-      Welcome to my presentation!
+      <div className={classes.table}>
+        <div className={classes.row}>
+          <span className={classes.rowLabel}>
+            一般 Selector：
+          </span>
+          <Selector />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/Views/Tools/Selector.jsx
@@ -0,0 +1,96 @@
+// @flow
+import React, { useState } from 'react';
+import { css, cx } from 'emotion';
+
+const classes = {
+  wrapper: css`
+    width: 100%;
+    height: 40px;
+    border: 1px solid #1a1a1a;
+    position: relative;
+  `,
+  activeWrapper: css`
+    border: 1px solid #ff0000;
+  `,
+  toggleBtn: css`
+    width: 100%;
+    height: 100%;
+    padding: 0 12px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    border: none;
+  `,
+  selectedValue: css`
+    font-size: 16px;
+    font-weight: 400;
+    color: #1a1a1a;
+    letter-spacing: 1px;
+  `,
+  modalWrapper: css`
+    position: absolute;
+    left: 0;
+    top: 40px;
+    width: 100%;
+    height: auto;
+    padding: 0 12px;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 12px;
+    box-shadow: 0px 0px 5px -1px rgba(0, 0, 0, 0.84);
+  `,
+  option: css`
+    width: 100%;
+    border: 0;
+    outline: 0;
+    background-color: transparent;
+    padding: 12px 0;
+    font-size: 16px;
+    color: #1a1a1a;
+    letter-spacing: 1px;
+  `,
+};
+
+const options = [{
+  id: '1',
+  name: '選項1',
+}, {
+  id: '2',
+  name: '選項2',
+}];
+
+function Selector() {
+  const [isModalOpen, toggleModalOpen] = useState(false);
+  const [selectedValue, setValue] = useState(null);
+
+  return (
+    <div className={cx(classes.wrapper, isModalOpen && classes.activeWrapper)}>
+      <button
+        type="button"
+        onClick={() => toggleModalOpen(prev => !prev)}
+        className={classes.toggleBtn}>
+        <span className={classes.selectedValue}>
+          {selectedValue || '請選擇'}
+        </span>
+      </button>
+      {isModalOpen ? (
+        <div className={classes.modalWrapper}>
+          {options.map(option => (
+            <button
+              key={option.id}
+              type="button"
+              onClick={() => {
+                setValue(option.name);
+                toggleModalOpen(false);
+              }}
+              className={classes.option}>
+              {option.name}
+            </button>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Selector;
